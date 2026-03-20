29b94870301d28f965d285d82946a4fea0cc9080
Richard Lampitt <richard@siruss.co.uk>
2026-03-20 16:56:26 +0100

    Remove more advertisements/upsells/nags.

--- admin-ad-sanitizer.php
+++ admin-ad-sanitizer.php
@@ -5,7 +5,7 @@ defined('ABSPATH') || exit; // exit if accessed directly.
 /*
  * Plugin Name: Admin Advertisement Sanitizer
  * Description: Hides obnoxious advertisements & upsells, notices hijacked for advertisements, and review nags in the administration area.
- * Version: 1.0.11.4
+ * Version: 1.0.11.5
  * License: GPL3+
  * Requires PHP: 7.4
  * Requires at least: 5.0
@@ -13,6 +13,7 @@ defined('ABSPATH') || exit; // exit if accessed directly.
 
 /*
  * Changelog:
+ * 1.0.11.5 - Added: Yoast external link in menu.
  * 1.0.11.4 - Bugfix: Incorrectly formatted CSS.
  * 1.0.11.3 - Added: Yoast disabled featured.
  * 1.0.11.2 - Bugfix: Overeager Selector affecting too many items.
